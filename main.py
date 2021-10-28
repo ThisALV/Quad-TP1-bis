b359d8c44b7cd4c28f1eb66c0fc39282365b7fcf
ThisALV <lel.charriere@orange.fr>
2021-10-28 09:53:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+from package_calculator import calculator
+
+print(f"2 + 3 = {calculator.somme(2, 3)}")
---
+++ tests/unit/package_calculator/test_calculator.py
@@ -0,0 +1,25 @@
+from package_calculator import calculator
+
+
+def test_calculator_somme():
+    assert calculator.somme(1, 1) == 2
+    assert calculator.somme(-3, 1) == -2
+    assert calculator.somme(4, -5) == -1
+
+
+def test_calculator_soustraction():
+    assert calculator.soustraction(1, 1) == 0
+    assert calculator.soustraction(17, -5) == 22
+    assert calculator.soustraction(-5, 5) == -10
+
+
+def test_calculator_multiplication():
+    assert calculator.multiplication(5, 1) == 5
+    assert calculator.multiplication(10, -2) == -20
+    assert calculator.multiplication(-6, 6) == -36
+
+
+def test_calculator_division():
+    assert calculator.division(5, 2) == 2.5
+    assert calculator.division(30, -3) == -10
+    assert calculator.division(-10, 4) == -2.5
