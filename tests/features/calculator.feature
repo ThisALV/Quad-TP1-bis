03f9952da879b88d56e6597d453648cc26518d99
ThisALV <lel.charriere@orange.fr>
2021-11-15 20:11:11 +0100

    Adds features testing for other operations

--- tests/features/calculator.feature
+++ tests/features/calculator.feature
@@ -11,3 +11,23 @@ Feature: calculator.calcule
         | -2 | 1  | -1               |
         | 2  | 3  | 5                |
         | 5  | 5  | 10               |
+
+  Scenario Outline: Différence entre deux nombres
+    Given Deux nombres <a> et <b>
+    When Le calculateur soustrait le 2ème terme au 1er
+    Then Le résultat est <resultat_attendu>
+    Examples:
+        | a  | b  | resultat_attendu |
+        | -2 | 1  | -3               |
+        | 3  | 2  | 1                |
+        | 5  | 5  | 0                |
+
+  Scenario Outline: Division d'un nombre par 0
+    Given Un nombre <x>
+    When Le calculateur divise le nombre par 0
+    Then Une erreur arithmétique est levée
+    Examples:
+        | x  |
+        | -5 |
+        | 1  |
+        | 0  |
--- tests/features/steps/calculator.py
+++ tests/features/steps/calculator.py
@@ -8,11 +8,35 @@ def step_impl(context, a, b):
     context.b = b
 
 
+@behave.given("Un nombre {x:d}")
+def step_impl(context, x):
+    context.x = x
+
+
 @behave.when("Le calculateur les additionne")
 def step_impl(context):
     context.resultat = calcule("+", context.a, context.b)
 
 
+@behave.when("Le calculateur soustrait le 2ème terme au 1er")
+def step_impl(context):
+    context.resultat = calcule("-", context.a, context.b)
+
+
+@behave.when("Le calculateur divise le nombre par 0")
+def step_impl(context):
+    try:
+        calcule("/", context.x, 0)
+        context.erreur_arithmetique = False
+    except ArithmeticError:
+        context.erreur_arithmetique = True
+
+
 @behave.then("Le résultat est {resultat_attendu:d}")
 def step_impl(context, resultat_attendu):
     assert resultat_attendu == context.resultat
+
+
+@behave.then("Une erreur arithmétique est levée")
+def step_impl(context):
+    assert context.erreur_arithmetique
