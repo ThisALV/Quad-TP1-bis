8b9304a0ac9b906cfd5ad3f507fdf52ddfac5adc
ThisALV <lel.charriere@orange.fr>
2021-11-18 09:25:56 +0100

    Setups features testing for front-end web

---
+++ tests/features/front-web.feature
@@ -0,0 +1,8 @@
+# language: fr
+Fonctionnalité: Front-end Web
+  Teste l'interface web de l'application calculator
+
+  Scénario:
+    Étant donné Un navigateur ouvert
+    Quand On charge une page web
+    Alors Elle s'affiche
---
+++ tests/features/steps/frontweb.py
@@ -0,0 +1,28 @@
+import behave
+from selenium import webdriver
+from selenium.webdriver.firefox import options as firefoxoptions
+
+
+@behave.given("Un navigateur ouvert")
+def step_impl(context):
+    options = firefoxoptions.Options()
+    options.headless = True
+    context.driver = webdriver.Firefox(options=options)
+
+
+titre_attendu = {
+    "https://www.google.com/": "Google",
+    "http://www.chezmoicamarche.org/": "Chez moi ça marche"
+}
+
+
+@behave.when("On charge une page web")
+def step_impl(context):
+    url = "http://www.chezmoicamarche.org/"
+    context.driver.get(url)
+    context.titre_attendu = titre_attendu[url]
+
+
+@behave.then("Elle s'affiche")
+def step_impl(context):
+    assert context.driver.title == context.titre_attendu
