fff0168fc4aedc2c1b392e732260f7beeca3ddf1
ThisALV <lel.charriere@orange.fr>
2021-11-22 09:34:42 +0100

    Feature testing for web server

--- tests/features/front-web.feature
+++ tests/features/front-web.feature
@@ -2,7 +2,7 @@
 Fonctionnalité: Front-end Web
   Teste l'interface web de l'application calculator
 
-  Plan du Scénario:
+  Plan du Scénario: Ouverture navigateur
     Étant donné Un navigateur ouvert
     Quand On charge une page web <url>
     Alors Elle s'affiche
@@ -10,3 +10,14 @@ Fonctionnalité: Front-end Web
       | url                               |
       | "https://www.google.com/"         |
       | "http://www.chezmoicamarche.org/" |
+
+  Plan du Scénario: Calculatrice
+    Étant donné La calculatrice ouverte dans un navigateur
+    Quand On rempli le formulaire avec <a>, <b> et on sélectionne l'opération <op>
+    Alors Elle affiche le calcul <calcul>
+    Exemples:
+      | a   | b   | op             | calcul        |
+      | 12  | -8  | Addition       | 12 + -8 = 4   |
+      | -4  | 10  | Soustraction   | -4 - 10 = -14 |
+      | 0   | 999 | Multiplication | 0 * 999 = 0   |
+      | 25  | 5   | Division       | 25 / 5 = 5.0  |
--- tests/features/steps/frontweb.py
+++ tests/features/steps/frontweb.py
@@ -1,6 +1,7 @@
 import behave
 from selenium import webdriver
 from selenium.webdriver.firefox import options as firefoxoptions
+from selenium.webdriver.support.ui import Select
 
 
 @behave.given("Un navigateur ouvert")
@@ -26,3 +27,31 @@ def step_impl(context, url):
 def step_impl(context):
     assert context.driver.title == context.titre_attendu
     context.driver.quit()
+
+
+@behave.given("La calculatrice ouverte dans un navigateur")
+def step_impl(context):
+    options = firefoxoptions.Options()
+    options.headless = True
+
+    context.driver = webdriver.Firefox(options=options)
+    context.driver.get("http://localhost:8080/index.html")
+
+
+@behave.when("On rempli le formulaire avec {a:d}, {b:d} et on sélectionne l'opération {op}")
+def step_impl(context, a, b, op):
+    context.driver.find_element(value="entree-a").send_keys(str(a))
+    context.driver.find_element(value="entree-b").send_keys(str(b))
+
+    choix_operation = Select(context.driver.find_element(value="operation"))
+    choix_operation.select_by_visible_text(op)
+
+    context.driver.find_element(value="valider").click()
+
+
+@behave.then("Elle affiche le calcul {calcul}")
+def step_impl(context, calcul):
+    try:
+        assert context.driver.page_source == f"<html><head></head><body>{calcul}</body></html>"
+    finally:
+        context.driver.quit()
